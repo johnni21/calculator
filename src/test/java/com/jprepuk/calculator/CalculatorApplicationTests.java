b67476ef0d9de1607e8fc32025512a1e8f7b52a1
Prepuk János <janos.prepuk@digi.hu>
2021-03-15 10:25:53 +0100

    Add Spring Boot skeleton

---
+++ src/test/java/com/jprepuk/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jprepuk.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
